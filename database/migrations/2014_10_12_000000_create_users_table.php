7b3baece7ccc42d9481f6f077006794b4ac83421
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-09-15 23:49:59 +0900

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            // $table->string('name');
+            // 変更
+            $table->string('name')->unique(); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            // $table->string('password');
+            // 変更
+            $table->string('password')->nullable(); 
             $table->rememberToken();
             $table->timestamps();
         });
