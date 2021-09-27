09c1665f6f06ca60b5b76d88ac21a19874776d76
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-09-27 21:14:34 +0900

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+// 追加
+use App\Article;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -9,41 +12,9 @@ class ArticleController extends Controller
 
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
-
-        return view('articles.index', ['articles' => $articles]);
+    // 追加
+    $articles = Article::all()->sortByDesc('created_at');
+    return view('articles.index', ['articles' => $articles]);
     }
 }
 
