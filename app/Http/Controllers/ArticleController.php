9846fe9a5842ecee4fcef5396d9089b1b59418c2
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-09-29 22:22:50 +0900

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,7 +13,22 @@ class ArticleController extends Controller
     public function index()
     {
     // 追加
-    $articles = Article::all()->sortByDesc('created_at');
+    // $articles = Article::all()->sortByDesc('created_at');
+
+    // ダミーデータ
+    $articles = [
+        (object) [
+            'id' => 1,
+            'title' => 'タイトル1',
+            'body' => '本文1',
+            'created_at' => now(),
+            'user' => (object) [
+                'id' => 1,
+                'name' => 'ユーザー名1',
+            ],
+        ],
+    ];
+
     return view('articles.index', ['articles' => $articles]);
     }
 }
