0caf2a5c8c766faa7470d08971bbbccb6c306c3b
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-06 23:12:44 +0900

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 // 追加
 use App\Article;
 
+// 追加
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -18,9 +21,20 @@ class ArticleController extends Controller
     return view('articles.index', ['articles' => $articles]);
     }
 
+    // createアクション
     public function create()
     {
         return view('articles.create');    
+    }
+
+    // storesアクション
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
 
