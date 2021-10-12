9688f550b0c45429f3276f51413c585a6d53d871
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-12 23:51:42 +0900

    Update index.blade.php

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -21,6 +21,7 @@
           </div>
         </div>
 
+        <!-- 認証済ユーザーのみ表示するようにしている@if~@endif -->
         @if( Auth::id() === $article->user_id )
           <!-- dropdown -->
           <div class="ml-auto card-text">
