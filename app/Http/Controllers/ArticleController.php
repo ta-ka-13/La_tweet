bef0aa0c585b2fa62ef93b2cb62511c605d23601
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-13 02:10:04 +0900

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -59,6 +59,12 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }    
+
 }
 
 
