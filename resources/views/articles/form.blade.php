c4cd1910789e2eed0ed15fb63fdf7eb7315017a3
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-13 00:03:03 +0900

    Update form.blade.php

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,6 +1,7 @@
 @csrf
 <div class="md-form">
   <label>タイトル</label>
+  <!-- ??はNull合体演算子 (式1 ?? 式2) 式1がnullでない場合は、式1が結果、式1がnullである場合は、式2が結果 -->
   <input type="text" name="title" class="form-control" required value="{{$article->title ?? old('title') }}">
 </div>
 <div class="form-group">
