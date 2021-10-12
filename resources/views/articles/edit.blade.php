eb740699f9c4c072934797c0b8ac62eccc99fd58
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-12 23:39:41 +0900

    Create edit.blade.php

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+
+@section('title', '記事更新')
+
+@include('nav')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="card mt-3">
+          <div class="card-body pt-0">
+            @include('error_card_list')
+            <div class="card-text">
+            <!-- formタグのaction属性に記事更新処理のURLを指定している -->
+            <!-- route関数の第二引数には、連想配列形式でルーティングのパラメーターを渡すことができる -->
+              <form method="POST" action="{{ route('articles.update', ['article' => $article]) }}">
+              <!-- Bladeで@method('PATCH')と記述するとBladeから生成されたHTMLではinputタグが埋め込まれる -->
+                @method('PATCH')
+                @include('articles.form')
+                <button type="submit" class="btn blue-gradient btn-block">更新する</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
