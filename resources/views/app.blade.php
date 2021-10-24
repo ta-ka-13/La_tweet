d8e0b7ea623713da487eaba1e6ab221468cc2b8f
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-10-25 01:41:16 +0900

    変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,10 +15,10 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body 
 
+  style="padding-top: 4.5rem;"> 
   @yield('content')
-
   <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
